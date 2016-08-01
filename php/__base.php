4cf091a254dcb0d996bfe6af211c0ec18250916b
马超 <chaoma0722@outlook.com>
2016-08-01 09:40:05 +0800

    remove bowerfile

--- php/__base.php
+++ php/__base.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lzsn
- * Date: 2016/7/1 0001
- * Time: 9:20
- */
 
 require_once '../lib/phpexcel/Classes/PHPExcel.php';
 
-$filename = "../excel/dataFilled.xlsx";
+$filename = "../excel/data.xlsx";
 
 $tableNames = ["DC1-TG现状配置表","DC1-SS现状配置表", "DC1-TG扩容需求表", "DC1-SS扩容需求表"];
 $PHPReader = new PHPExcel_Reader_Excel2007();
